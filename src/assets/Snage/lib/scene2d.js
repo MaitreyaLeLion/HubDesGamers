849b5b76a82f5a5804fb908e733b70961381f34d
MaitreyaLeLion <160273182+MaitreyaLeLion@users.noreply.github.com>
2024-05-30 12:31:52 +0200

    Update scene2d.js

--- src/assets/Snage/lib/scene2d.js
+++ src/assets/Snage/lib/scene2d.js
@@ -1,9 +1,9 @@
-import { createSnake } from "/Snage/Assets/lib/snake.js";
+import { createSnake } from "/src/assets/Snage/lib/snake.js";
 import {
   createGround2DMaterial,
   createAppleMaterial,
-} from "/Snage/Assets/lib/materials.js";
-import * as BABYLON from "/../../node_modules/@babylonjs/core";
+} from "/src/assets/Snage/lib/materials.js";
+import * as BABYLON from "/node_modules/@babylonjs/core";
 
 class Scene2D {
   constructor(engine, taille) {
